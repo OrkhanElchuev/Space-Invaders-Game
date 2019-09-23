9532dfb6a9b61c345bae9ae7cef028c8230e6305
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-09-23 17:35:48 +0200

    infinity scrolling background effect added

---
+++ Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    [SerializeField] float BgScrollingSpeed = 1.0f;
+    private Material bgMaterial;
+    private Vector2 offSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bgMaterial = GetComponent<Renderer>().material;
+        offSet = new Vector2(0f, BgScrollingSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bgMaterial.mainTextureOffset += offSet * Time.deltaTime;
+    }
+}
