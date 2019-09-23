12031f4cb27c60d9ac34bc982a43c54052fa0534
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-09-23 18:34:55 +0200

    Player sprites added, assets organized

--- Assets/Scripts/BackgroundScroll.cs
+++ Assets/Scripts/BackgroundScroll.cs
@@ -12,12 +12,14 @@ public class BackgroundScroll : MonoBehaviour
     void Start()
     {
         bgMaterial = GetComponent<Renderer>().material;
+        // Scrolling up on Y axis
         offSet = new Vector2(0f, BgScrollingSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Applying frame rate independent moving speed
         bgMaterial.mainTextureOffset += offSet * Time.deltaTime;
     }
 }
