77477152abacbd5caaf52c665f5ae9979ef3f9ad
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-09-23 23:00:06 +0200

    4 types of enemies added

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Enemy Stats")]
+    [SerializeField] int healthOfEnemy = 100;
+    [SerializeField] int scoreValue = 100;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
