9e8bc6b72e68eca1adefb41999f2a45592486d05
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-09-24 09:12:31 +0200

    Scene loader is added, Game Over and Start Menus created

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    public void LoadGamePlay()
+    {
+        SceneManager.LoadScene("GamePlay");
+    }
+
+    // Quit the application 
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
