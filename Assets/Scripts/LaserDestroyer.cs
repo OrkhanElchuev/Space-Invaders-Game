25228610435e4ee4a3c30e7bba55d8c259cbda9e
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-09-24 08:55:10 +0200

    Laser Shredder to destroy objects going out of screen

---
+++ Assets/Scripts/LaserDestroyer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserDestroyer : MonoBehaviour
+{
+    // Destroy redundant objects(Lasers) in case of going out of screen
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Destroy(collider.gameObject);
+    }
+}
