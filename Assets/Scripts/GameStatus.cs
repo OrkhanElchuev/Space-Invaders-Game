021dfe24b0e175df8f438eb85d13819b4fd35072
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-09-24 14:06:12 +0200

    Game status script added to handle score addition

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+    private int playerScore = 0;
+
+    private void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    private void SetUpSingleton()
+    {
+        // If there are more than one GameStatus objects destroy itself
+        int numGameSessions = FindObjectsOfType<GameStatus>().Length;
+        if (numGameSessions > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Get score value 
+    public int GetScore()
+    {
+        return playerScore;
+    }
+
+    // Add to score value
+    public void AddToScore(int scoreValue)
+    {
+        playerScore += scoreValue;
+    }
+
+    // Destroy GameStatus object
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+}
