0d7da03cd424b8763e7a5dd66dd5e455fd565fc1
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-09-29 22:22:38 +0200

    Scoreboard testing text field created, valus from scoreboard assigned to text field

--- Assets/Scripts/PlayerScript/ScoreBoard.cs
+++ Assets/Scripts/PlayerScript/ScoreBoard.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.UI;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject PlayerInfo;
+    [SerializeField] TextMeshProUGUI[] leaderBoardTextArr;
+    private PlayerInfo playerInfoScript;
+    private void Awake()
     {
-        
+        playerInfoScript = PlayerInfo.GetComponent<PlayerInfo>();
+        playerInfoScript.LoadPlayer();
+        SettingTextValues();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    // Fill the values of Text component with values from score board array
+    private void SettingTextValues(){
+        int[] scoreBoardArr = playerInfoScript.GetScoreBoard();
+        for (int i = 0; i < scoreBoardArr.Length; i++)
+        {
+            leaderBoardTextArr[i].text = scoreBoardArr[i].ToString();
+        }
     }
 }
