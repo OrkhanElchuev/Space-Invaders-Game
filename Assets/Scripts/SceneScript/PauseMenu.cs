0624a7b9d7c706f4f7b8a1663ad89f97dbc4b5d0
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-09-30 16:50:14 +0200

    Pause menu functionality added

--- Assets/Scripts/SceneScript/PauseMenu.cs
+++ Assets/Scripts/SceneScript/PauseMenu.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    [SerializeField] SceneLoader sceneLoader;
 
-    // Update is called once per frame
+    // When escape key pressed, pause or resume the game
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        // Make pause panel inactive
+        pauseMenuUI.SetActive(false);
+        // Resume the game flow
+        Time.timeScale = 1.0f;
+        GameIsPaused = false;
+    }
+
+    // Load start menu and resume the game flow
+    public void LoadStartMenu()
+    {
+        Time.timeScale = 1.0f;
+        sceneLoader.LoadStartMenu();
+    }
+
+    private void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        // Freeze the game flow
+        Time.timeScale = 0f;
+        GameIsPaused = true;
     }
 }
